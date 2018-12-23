e5ad906731a11e1924fe8fff864e30bfafcdbefa
Nikolai Flowers <nikolaif399@gmail.com>
2018-12-22 21:46:04 -0800

    Added mandelbrot framework

--- libs/mandelbrot.py
+++ libs/mandelbrot.py
@@ -1,14 +1,14 @@
 
 """
 returns number of iterations if can escape (>2)
-in less than 100 iterations, else returns NaN
+in less than 100 iterations, else returns None
 """
-def mandelbrot(row, col):
-	c = complex(row, col) #complex(real, complex)
-	z = 0
+def iterate(y, x):
+	c = complex(y, x) #complex(real, complex)
+	z = 0.1
 	for n in range(1, 100):
 		z = z**2 + c
 		if abs(z) > 2:
 			return n
-	return NaN
+	return None
 
--- src/main.py
+++ src/main.py
@@ -5,14 +5,41 @@ import mandelbrot
 
 class Application(object):
 	def __init__(self):
+		self.width = 500
+		self.height = 500
 		self.setup_root()
+
+		self.current_window = (-1, 1, -1, 1)
 
 	def setup_root(self):
 		root = Tk()
-		w = Label(root, text="Hello, world!")
-		w.pack()
+		canvas = Canvas(root, width = self.width, height = self.height)
+		canvas.pack()
 
-		self.root = root #hold on to root reference
+		root.bind("<Key>", lambda event:
+                            self.key_pressed(event))
+
+		#hold on to references
+		self.root = root
+		self.canvas = canvas
+
+	def key_pressed(self, event):
+		self.mandel_update()
+
+	def mandel_update(self):
+		a = []
+		row_range = self.current_window[1] - self.current_window[0]
+		col_range = self.current_window[3] - self.current_window[2]
+		for row in range(self.height):
+			for col in range(self.width):
+				i = 2 * row / (row_range) - 1 #shift to current window
+				j = 2 * col / (col_range) - 1
+				n = mandelbrot.iterate(i, j)
+				a.append(n)
+
+		print (max(a))
+		print (min(a))
+
 
 	def run(self):
 		self.root.mainloop()
